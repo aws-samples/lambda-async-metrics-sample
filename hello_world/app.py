2bd976274e6621bda3f5b95a2b006691892367ec
Dhiraj Mahapatro <mahadhir@amazon.com>
2023-01-27 16:44:11 -0500

    Remove commented code for clarity

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,9 +1,5 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-# import time
-
 def lambda_handler(event, context):
-    # time.sleep(90)
     print("Hello World from AWS Lambda")
-    # raise Exception("Exception from Lambda function")
